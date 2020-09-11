7f44a870adce9851902d8a04eb939470e3b6a280
Andreas-Ravn <andr8343@edu.easj.dk>
2020-09-11 11:00:52 +0200

    Tilføjet navn "LEEEEEROOOYYY"

--- HelloJava.java
+++ HelloJava.java
@@ -3,6 +3,7 @@ public class HelloJava {
         System.out.println("Hello Java!");
         System.out.println("This works!");
         System.out.println("boo and what");
+        System.out.println("LEEEEROOYYY");
     }
 
 }
