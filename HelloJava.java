50331699c85c863920187f126dfb8990a26eecc3
Anders H. Jørgensen <38241145+virtualzeth@users.noreply.github.com>
2020-10-01 12:38:02 +0200

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,6 @@
 public class HelloJava {
     public static void main(String[] args) {
-        //System.out.println() is used for printing the text on console screen
+        // System.out.println() is used for printing the text on console screen
         // and after writing the text it will go to next line automatically.
         System.out.println("Hello Java!");
         System.out.println("This works!");
